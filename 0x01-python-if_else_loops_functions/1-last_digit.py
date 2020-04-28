78d51662389edb765b4d40f79ffdf00ceddcccc2
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-04-28 18:13:58 +0000

    adding task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ if number < 0:
 	last_digit = _number % 10
 
 	if last_digit == 0:
-		print("last_digit of -%d is %d and is 0" %(_number, last_digit))
+		print("Last digit of -%d is %d and is 0" %(_number, last_digit))
 
 	else:
-		print("last_digit of -%d is -%d and is less than 6 and not 0" %(_number, last_digit))
+		print("Last digit of -%d is -%d and is less than 6 and not 0" %(_number, last_digit))
 
 else:
 	last_digit = number % 10
