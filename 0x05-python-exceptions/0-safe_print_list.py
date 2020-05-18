7637c2ec8446fc9e787bde060178a7fddb8f2233
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-05-18 22:55:16 +0000

    Corrected end space in print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         for i in range(x):
-            print(my_list[i], end = "")
+            print(my_list[i], end="")
             i += 1
         print()
     except IndexError:
