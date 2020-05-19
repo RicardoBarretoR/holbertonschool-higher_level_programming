60ef6cea27cbc070851b4d883338a977e6b84791
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-05-19 00:32:09 +0000

    Deleted conditional if

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     """"prints an integer with "{:d}".format()"""
     try:
-        if value % 1 == 0:
-            print("{:d}".format(value))
+        print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
         return False
