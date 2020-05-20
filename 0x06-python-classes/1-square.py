ca609b8a81e1958ccb85cda4ba427981dc090f5b
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-05-20 01:53:04 +0000

    adding task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class Square that defines a square with size"""
+
+
+class Square:
+    """passing initial values to object"""
+    def __init__(self, size):
+
+        self.__size = size
