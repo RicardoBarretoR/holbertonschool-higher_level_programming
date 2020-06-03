e9a02660cdbaac52a3e0842f937529ac3a860a38
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-06-03 03:36:43 +0000

    adding task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that returns True
+if the object is an instance
+or instance of a class that inherited
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    '''module 3-is_kin_of_class'''
+
+    return isinstance(obj, a_class)
