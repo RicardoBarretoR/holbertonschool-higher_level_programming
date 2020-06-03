89dc62200853940d7fa3fe207b8626bb2dc0453a
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-06-03 04:09:58 +0000

    fixing task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
 function that returns True if the object is an
-instance of a class that inherited (directly or indirectly)
+instance of a class that inherited
 from the specified class
 
 """
 
 
 def inherits_from(obj, a_class):
-    'module 4-inherits_from'
-    return issubclass(type(obj, a_class) and type(obj is not a_class))
+    """module 4-inherits_from"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
