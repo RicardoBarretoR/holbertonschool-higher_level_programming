2b878e44e7160cb16dc9d3f3c9290f437078b9ab
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-06-09 23:51:16 +0000

    adding task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,18 @@ class Rectangle(Base):
         'redefine the method and return the string'
         return ("[{}] ({}) {}/{} - {}/{}".format(__class__.__name__,
                 self.id, self.x, self.y, self.width, self.height))
+
+    def update(self, *args):
+        'assigns an argument to each attribute'
+        if len(args) > 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                if i == 2:
+                    self.height = args[2]
+                if i == 3:
+                    self.x = args[3]
+                if i == 4:
+                    self.y = args[4]
