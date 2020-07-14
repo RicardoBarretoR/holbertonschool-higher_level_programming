bec5a854d9155bbe5822191ce070b814e5b2591d
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-07-14 09:37:11 +0000

    adding task 10: lists all records of the table second_table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+-- lists all records of the table second_table
+-- Results should display both the score and the name in order
+SELECT score, name FROM second_table ORDER BY score DESC;
