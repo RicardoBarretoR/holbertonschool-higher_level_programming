6a429870bdc89c01adce06138390143b2db2b21d
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-07-15 09:26:16 +0000

    fixing states_id for state_id

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 --  lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.states_id=states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
