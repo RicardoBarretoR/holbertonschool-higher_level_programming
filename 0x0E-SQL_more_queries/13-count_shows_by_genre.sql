034b9832c1e4f1f558891f6afeda1d34f828183c
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-07-16 08:42:43 +0000

    adding task 13:lists all genres from hbtn_0d_tvshows and number

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
---lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
     FROM tv_genres JOIN tv_show_genres
     ON tv_genres.id = tv_show_genres.genre.id
