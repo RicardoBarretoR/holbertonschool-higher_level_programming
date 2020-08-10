74beb403faa05c2033cd88b907ad1f877f4d44fc
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-08-10 07:14:32 +0000

    fixing task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-# script that lists all states from the database
+"""script that lists all states from the database"""
+import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Establish connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user="root",
-                         passwd="root", db="hbtn_0e_0_usa")
-    # create a cursor by executing the 'cursor' function of database
+    """Establish connection to the database"""
+    user = sys.argv[1]
+    password = sys.argv[2]
+    basedata = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=basedata)
+    """create a cursor by executing the 'cursor' function of database"""
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
