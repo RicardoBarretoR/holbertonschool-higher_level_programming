e92232c585c7da99b59904f46c9dd49f123d2149
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-08-10 12:00:17 +0000

    fixing task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,15 +8,15 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     basedata = sys.argv[3]
-    state = sys.argv[4]
+
     db = MySQLdb.connect(host="localhost", port=3306, user=user,
                          passwd=password, db=basedata)
     """create a cursor by executing the 'cursor' function of database"""
     cur = db.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name
-    FROM cities, states
-    WHERE cities.state_id = states.id
-    ORDER BY cities.id ASC;""")
+                FROM cities, states
+                WHERE cities.state_id = states.id
+                ORDER BY cities.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
