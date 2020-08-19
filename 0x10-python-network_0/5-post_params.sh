b076b589e04bf507c7ced85c5d4e95f7db32e2dd
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-08-18 23:02:15 -0500

    fixing task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,4 +2,4 @@
 #A URL, send a POST request and display the body of the response
 #variable email must be sent with the value hr@holbertonschool.com
 #variable subject must be sent with the value I will always be here for PLD
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
