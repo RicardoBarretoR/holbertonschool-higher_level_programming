840ff5fe11f5e7937d18e9df21416cc57e22541f
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-01 23:55:47 -0500

    fixing task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,14 @@
 """ python script that fetches a web
 
 """
-import urllib.request
-
 if __name__ == "__main__":
+    import urllib.request
     url = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
-        body = response.read()
-        content = body.decode('utf-8')
+        bytes_content = response.read()
+        content = bytes_content.decode('utf-8')
         print_str = '''Body response:
-    \t- type:{}
-    \t- content:{}
-    \t- utf8 content: {}'''.format(type(body), body, content)
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
         print(print_str)
