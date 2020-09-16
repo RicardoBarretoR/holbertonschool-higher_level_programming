d37a2db9e5019e7358019a0a64c8ea6124e395eb
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-16 11:57:14 +0000

    adding task 7: prints x times C is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+  console.log('Missing number of occurrences');
+} else if (parseInt(process.argv[2]) > 0) {
+  console.log('C is fun\n'.repeat(parseInt(process.argv[2])).slice(0, -1));
+}
+while (1) {
+  process.exit();
+}
