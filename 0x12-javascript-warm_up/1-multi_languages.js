d89a5e94cdbeb0a79a2ab5394606e8a71992cfc9
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-16 06:24:43 +0000

    adding task 1:prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let lenguages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lenguages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (i = 0; i < lenguages.length; i++) {
   console.log(lenguages[i]);
 }
