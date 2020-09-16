fbd5e2974423c2fd212ae76e0e728b645bde2510
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-15 23:36:36 -0500

    fixing character X

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const integer = parseInt(process.argv.slice(2)[0]);
 if (!isNaN(integer)) {
   for (let iter = 0; iter < integer; iter++) {
-    console.log('x'.repeat(integer));
+    console.log('X'.repeat(integer));
   }
 } else {
   console.log('Missing size');
