f306ab2c90dc5aa5cf636860f326e8c70c0b2a31
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-21 23:36:29 -0500

    adding task advanced 102: concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+fs.readFile(fileA, 'utf8', function (err, data) {
+  if (err) throw err;
+  fs.writeFile(fileC, data, function (err) {
+    if (err) throw err;
+  });
+});
+
+fs.readFile(fileB, 'utf8', function (err, data) {
+  if (err) throw err;
+  fs.appendFile(fileC, data, function (err) {
+    if (err) throw err;
+  });
+});
