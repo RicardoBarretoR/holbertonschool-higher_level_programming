616739f71e144e4ce88125fd4336f03955939125
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-22 23:48:54 -0500

    adding task 6: computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) { console.log(err); return; }
+
+  const tasks = JSON.parse(body);
+  const tasksCompleted = {};
+
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (tasksCompleted[task.userId] === undefined) {
+        tasksCompleted[task.userId] = 1;
+      } else {
+        tasksCompleted[task.userId] += 1;
+      }
+    }
+  });
+
+  console.log(tasksCompleted);
+});
