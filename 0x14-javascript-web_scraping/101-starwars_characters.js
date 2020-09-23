a25685fe7e8a2203ccfd1e471fbaf43e086bc1d0
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-22 23:58:30 -0500

    adding task advenced 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+request(url, function (err, response, body) {
+  if (err) { console.log(err); return; }
+
+  const characters = JSON.parse(body).characters;
+  const i = characters.entries();
+  for (const e of i) {
+    request(e[1], function (err, response, body) {
+      if (err) { console.log(err); return; }
+      const character = JSON.parse(body).name;
+      console.log(character);
+    });
+  }
+
+  /* for (const character of characters) {
+    request(character, function (err, response, body) {
+      if (err) { console.log(err); return; }
+      const character = JSON.parse(body).name;
+      console.log(character);
+    });
+  } */
+});
